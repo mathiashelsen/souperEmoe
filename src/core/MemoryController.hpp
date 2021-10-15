495b637c021218ec64938e0f214f64c7f882bed3
Mathias Helsen <helsen.mathias@gmail.com>
2021-10-15 09:03:24 +0200

    Adding a layer between CPU and Memory/Video to direct the datastream

---
+++ src/core/MemoryController.hpp
@@ -0,0 +1,23 @@
+#ifndef _MEMORY_CONTROLLER_HPP
+#define _MEMORY_CONTROLLER_HPP
+
+#include <stdint.h>
+
+#include "Video.hpp"
+#include "Memory.hpp"
+
+#include "../aux/c64_constants.hpp"
+
+class MemoryController
+{
+  protected:
+    Video*    _video;
+    Memory*   _memory;
+  public:
+    MemoryController(Video* video, Memory* memory);
+    uint8_t   read  (uint16_t address);
+    void      write (uint16_t address, uint8_t data);
+
+};
+
+#endif
